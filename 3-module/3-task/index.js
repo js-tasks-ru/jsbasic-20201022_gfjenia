02d8db70f73bc24ced375704b8dd105cde58e985
Eugene Sulitsenko <gfjenia@gmail.com>
2020-10-29 16:17:26 +0300

    Added tasks in 3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,20 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  if (str === "") {
+    return str;
+  }
+  
+  let camelized = [];
+  let words = str.split("-");
+
+  camelized.push(words[0]);
+
+  for (let index = 1; index < words.length; index++) {
+    if (words[index].length > 0) {
+      camelized.push(words[index][0].toUpperCase() + words[index].slice(1));
+    }
+  }
+
+  return camelized.join('');
 }
